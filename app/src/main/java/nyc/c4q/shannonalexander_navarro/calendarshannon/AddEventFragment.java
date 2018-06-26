3f0d90dfbfa8082e74c82b03b209e13ff65030bb
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2018-06-25 23:24:15 -0400

    refactor, spark, firebase

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/calendarshannon/AddEventFragment.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/calendarshannon/AddEventFragment.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 /**
  * Created by shannonalexander-navarro on 6/21/18.
  */
@@ -17,6 +23,7 @@ public class AddEventFragment extends Fragment {
 
     private Button submitBtn;
     private int date;
+    Database database = new Database();
 
     @Nullable
     @Override
@@ -45,6 +52,30 @@ public class AddEventFragment extends Fragment {
 
     private void closeFragment() {
         getActivity().getSupportFragmentManager().beginTransaction().remove(this).commit();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("stuff");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("TAG", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    private void read(){
+
     }
 
 
