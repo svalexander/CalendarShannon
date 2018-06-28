46031750fd08f44786ca0705dc09e3191344d3d4
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2018-06-28 01:53:55 -0400

    updated service class

--- app/src/main/java/nyc/c4q/shannonalexander_navarro/calendarshannon/CalendarBackEnd/EventService.java
+++ app/src/main/java/nyc/c4q/shannonalexander_navarro/calendarshannon/CalendarBackEnd/EventService.java
@@ -1,20 +1,15 @@
 package nyc.c4q.shannonalexander_navarro.calendarshannon.CalendarBackEnd;
 
-import java.util.HashMap;
-
 import nyc.c4q.shannonalexander_navarro.calendarshannon.models.Event;
 
 public class EventService {
-
-    private HashMap<Integer,Event> eventMap = new HashMap<>();
-
+    
     public Event getEvent(){
         return new Event();
     }
 
     public Event createEventToPost(int date, String startTime, String endTime, String description){
         Event event = new Event(date, startTime, endTime, description);
-        eventMap.put(event.getDate(),event);
         return event;
     }
 
